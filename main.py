b105cb653ce8655ca6303a66416aeaa398f9ef44
BrotatoBoiV2 <flamingburrito15@gmail.com>
2026-02-02 13:06:52 -0500

    Updated filestamps

--- main.py
+++ main.py
@@ -1,4 +1,33 @@
-# 2026/01/28
+"""
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+                            Company: SpudWorks.
+                        Program Name: Live Translate.
+      Description: A helpful AI Assistent that act as the host device.
+                              File: main.py
+                            Date: 2026/01/28
+                        Version: 0.1-2026.02.02
+
+===============================================================================
+
+                        Copyright (C) 2026 SpudWorks Labs.
+
+        This program is free software: you can redistribute it and/or modify
+        it under the terms of the GNU Affero General Public License as published
+        by the Free Software Foundation, either version 3 of the License, or
+        (at your option) any later version.
+
+        This program is distributed in the hope that it will be useful,
+        but WITHOUT ANY WARRANTY; without even the implied warranty of
+        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+        GNU Affero General Public License for more details.
+
+        You should have received a copy of the GNU Affero General Public License
+        along with this program. If not, see <https://www.gnu.org/licenses/>
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+"""
+
+
 from blessed import Terminal
 from time import sleep
 
