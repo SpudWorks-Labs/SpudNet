cb932b0f5e2aeca020233cdbcea9f0f87b724115
BrotatoBoiV2 <flamingburrito15@gmail.com>
2026-02-04 11:56:05 -0500

    Added an error catcher.

--- server.py
+++ server.py
@@ -85,4 +85,7 @@ async def get_history():
 if __name__ == "__main__":
     print(">>> SpudNet Daemon Online at http://127.0.0.1:8000")
 
-    uvicorn.run(app, host="127.0.0.1", port=8000, log_level="info")
+    try:
+        uvicorn.run(app, host="127.0.0.1", port=8000, log_level="info")
+    except Exception as e:
+        print("There has been an error: {e}")
